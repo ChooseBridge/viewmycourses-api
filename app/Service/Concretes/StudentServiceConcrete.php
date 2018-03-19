9c206c83d62ce521f6118992f3dab49efb71a00d
pony <835271106@qq.com>
2018-03-19 20:42:18 +0800

    调试代码

--- app/Service/Concretes/StudentServiceConcrete.php
+++ app/Service/Concretes/StudentServiceConcrete.php
@@ -10,14 +10,15 @@ namespace App\Service\Concretes;
 
 use App\Service\Abstracts\StudentServiceAbstract;
 use App\Student;
+use GuzzleHttp\Exception\ClientException;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\App;
 
 
 class StudentServiceConcrete implements StudentServiceAbstract
 {
-    const GET_POINTS_URL  = "api/user/points/get";
-    const SET_POINTS_URL  = "api/user/points/set";
+    const GET_POINTS_URL = "api/user/points/get";
+    const SET_POINTS_URL = "api/user/points/set";
 
     public function createStudent($data)
     {
@@ -52,37 +53,50 @@ class StudentServiceConcrete implements StudentServiceAbstract
 
     public function setPoints($delta)
     {
-        $client = new \GuzzleHttp\Client([
-          'base_uri' => env('UCENTER_URL')
-        ]);
-        $response = $client->request('PUT', env('UCENTER_URL') . self::SET_POINTS_URL, [
-          'json'=>[
-            'access_token'=>$GLOBALS['gStudent']->access_token,
-            'delta'=>$delta,
-          ]
-        ]);
-        $content = $response->getBody()->getContents();
-        $pointsInfo = json_decode($content, true);
-        if($pointsInfo['success']){
-            return $pointsInfo['entities'];
+        try {
+            $client = new \GuzzleHttp\Client([
+              'base_uri' => env('UCENTER_URL')
+            ]);
+            $response = $client->request('PUT', env('UCENTER_URL') . self::SET_POINTS_URL, [
+              'json' => [
+                'access_token' => $GLOBALS['gStudent']->access_token,
+                'delta' => $delta,
+              ]
+            ]);
+            $content = $response->getBody()->getContents();
+            $pointsInfo = json_decode($content, true);
+            if ($pointsInfo['success']) {
+                return $pointsInfo['entities'];
+            }
+
+        } catch (ClientException $exception) {
+            var_dump($exception->getCode());
+            die;
         }
+
     }
 
     public function getPoints()
     {
-        $client = new \GuzzleHttp\Client([
-          'base_uri' => env('UCENTER_URL')
-        ]);
-        $response = $client->request('GET', env('UCENTER_URL') . self::GET_POINTS_URL, [
-          'query'=>[
-            'access_token'=>$GLOBALS['gStudent']->access_token
-          ]
-        ]);
-        $content = $response->getBody()->getContents();
-        $pointsInfo = json_decode($content, true);
-        if($pointsInfo['success']){
-            return $pointsInfo['entities']['points'];
+        try {
+            $client = new \GuzzleHttp\Client([
+              'base_uri' => env('UCENTER_URL')
+            ]);
+            $response = $client->request('GET', env('UCENTER_URL') . self::GET_POINTS_URL, [
+              'query' => [
+                'access_token' => $GLOBALS['gStudent']->access_token
+              ]
+            ]);
+            $content = $response->getBody()->getContents();
+            $pointsInfo = json_decode($content, true);
+            if ($pointsInfo['success']) {
+                return $pointsInfo['entities']['points'];
+            }
+        } catch (ClientException $exception) {
+            var_dump($exception->getCode());
+            die;
         }
+
     }
 
 
