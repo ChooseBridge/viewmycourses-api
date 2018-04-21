8004dd65a03097db395e4de54825d0b66dc24306
pony <835271106@qq.com>
2018-04-21 16:56:54 +0800

    去掉修正功能

--- resources/views/school/index.blade.php
+++ resources/views/school/index.blade.php
@@ -74,8 +74,8 @@
                         <a class="btn btn-info"
                            href="{{route('backend.school.update.get',['school_id' => $school->school_id])}}">编辑</a>
 
-                        <a class="btn btn-success"
-                           href="{{route('backend.school.show-comment.get',['school_id' => $school->school_id])}}">修正</a>
+                        {{--<a class="btn btn-success"--}}
+                           {{--href="{{route('backend.school.show-comment.get',['school_id' => $school->school_id])}}">修正</a>--}}
 
                     @if($school->check_status == \App\School::PENDING_CHECK)
                             <a class="btn btn-success"
