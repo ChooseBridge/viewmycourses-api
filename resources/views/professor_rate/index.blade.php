26f67001be34ca1cc7729c3dcfddd62decbbd7b4
pony <835271106@qq.com>
2018-05-06 15:49:36 +0800

    后台显示修改字段

--- resources/views/professor_rate/index.blade.php
+++ resources/views/professor_rate/index.blade.php
@@ -48,15 +48,15 @@
                 <th>课程代码</th>
                 <th>课程名称</th>
                 <th>课程类别</th>
-                <th>是否出勤</th>
-                <th>课程难度</th>
-                <th>笔头作业量</th>
-                <th>每月考试数</th>
-                <th>课程与考试内容相关度</th>
-                <th>每周课堂外所花总时间</th>
-                <th>你的成绩</th>
-                <th>文字点评</th>
-                <th>标签</th>
+                {{--<th>是否出勤</th>--}}
+                {{--<th>课程难度</th>--}}
+                {{--<th>笔头作业量</th>--}}
+                {{--<th>每月考试数</th>--}}
+                {{--<th>课程与考试内容相关度</th>--}}
+                {{--<th>每周课堂外所花总时间</th>--}}
+                {{--<th>你的成绩</th>--}}
+                {{--<th>文字点评</th>--}}
+                {{--<th>标签</th>--}}
                 <th>点评学生</th>
                 <th>审核状态</th>
                 <th>操作</th>
@@ -70,15 +70,15 @@
                     <td>{{$rate->course_code}}</td>
                     <td>{{$rate->course_name}}</td>
                     <td>{{$rate->course_category_name}}</td>
-                    <td>{{$rate->attend}}</td>
-                    <td>{{$rate->difficult_level}}</td>
-                    <td>{{$rate->homework_num}}</td>
-                    <td>{{$rate->quiz_num}}</td>
-                    <td>{{$rate->course_related_quiz}}</td>
-                    <td>{{$rate->spend_course_time_at_week}}</td>
-                    <td>{{$rate->grade}}</td>
-                    <td>{{$rate->comment}}</td>
-                    <td>{{$rate->tag}}</td>
+                    {{--<td>{{$rate->attend}}</td>--}}
+                    {{--<td>{{$rate->difficult_level}}</td>--}}
+                    {{--<td>{{$rate->homework_num}}</td>--}}
+                    {{--<td>{{$rate->quiz_num}}</td>--}}
+                    {{--<td>{{$rate->course_related_quiz}}</td>--}}
+                    {{--<td>{{$rate->spend_course_time_at_week}}</td>--}}
+                    {{--<td>{{$rate->grade}}</td>--}}
+                    {{--<td>{{$rate->comment}}</td>--}}
+                    {{--<td>{{$rate->tag}}</td>--}}
                     <td>{{$rate->student->name}}</td>
                     <td>{{$rate->checkStatusName}}</td>
                     <td>
--- resources/views/school_rate/index.blade.php
+++ resources/views/school_rate/index.blade.php
@@ -42,17 +42,18 @@
         <table class="table table-bordered">
             <tr>
                 <th>点评id</th>
+                <th>学校</th>
                 <th>校区</th>
-                <th>社会声誉</th>
-                <th>学术水平</th>
-                <th>网络服务</th>
-                <th>住宿条件</th>
-                <th>餐饮质量</th>
-                <th>校园地理位置</th>
-                <th>校园课外活动</th>
-                <th>校园基础设施</th>
-                <th>生活幸福指数</th>
-                <th>校方与学生群体关系</th>
+                {{--<th>社会声誉</th>--}}
+                {{--<th>学术水平</th>--}}
+                {{--<th>网络服务</th>--}}
+                {{--<th>住宿条件</th>--}}
+                {{--<th>餐饮质量</th>--}}
+                {{--<th>校园地理位置</th>--}}
+                {{--<th>校园课外活动</th>--}}
+                {{--<th>校园基础设施</th>--}}
+                {{--<th>生活幸福指数</th>--}}
+                {{--<th>校方与学生群体关系</th>--}}
                 {{--<th>文字点评</th>--}}
                 <th>点评学生</th>
                 <th>审核状态</th>
@@ -62,17 +63,18 @@
             @foreach ($rates as $rate)
                 <tr>
                     <td>{{$rate->school_rate_id}}</td>
+                    <td>{{$rate->school->school_name}}</td>
                     <td>{{$rate->schoolDistrict->school_district_name}}</td>
-                    <td>{{$rate->social_reputation}}</td>
-                    <td>{{$rate->academic_level}}</td>
-                    <td>{{$rate->network_services}}</td>
-                    <td>{{$rate->accommodation}}</td>
-                    <td>{{$rate->food_quality}}</td>
-                    <td>{{$rate->campus_location}}</td>
-                    <td>{{$rate->extracurricular_activities}}</td>
-                    <td>{{$rate->campus_infrastructure}}</td>
-                    <td>{{$rate->life_happiness_index}}</td>
-                    <td>{{$rate->school_students_relations}}</td>
+                    {{--<td>{{$rate->social_reputation}}</td>--}}
+                    {{--<td>{{$rate->academic_level}}</td>--}}
+                    {{--<td>{{$rate->network_services}}</td>--}}
+                    {{--<td>{{$rate->accommodation}}</td>--}}
+                    {{--<td>{{$rate->food_quality}}</td>--}}
+                    {{--<td>{{$rate->campus_location}}</td>--}}
+                    {{--<td>{{$rate->extracurricular_activities}}</td>--}}
+                    {{--<td>{{$rate->campus_infrastructure}}</td>--}}
+                    {{--<td>{{$rate->life_happiness_index}}</td>--}}
+                    {{--<td>{{$rate->school_students_relations}}</td>--}}
                     {{--<td>{{$rate->comment}}</td>--}}
                     <td>{{$rate->student->name}}</td>
                     <td>{{$rate->checkStatusName}}</td>
